5b4f245108af751bbd9b4616dc189095baaabac3
Asia Kane and Jennifer Kinsey <asia@>
2017-04-27 16:21:08 -0700

    complete passing integration specs

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -15,6 +15,23 @@ describe("the contact path", {:type => :feature}) do
     visit("/contacts/new")
     fill_in("first_name", :with => "John")
     click_button("Add Contact")
-    expect(page).to have_content("'madam' is a palindrome")
+    expect(page).to have_content("You've successfully")
+    click_link("Back to contact list")
+    expect(page).to have_content("John")
   end
+
+  it("goes to the new contact page") do
+    visit("/contacts")
+    click_link("Back to homepage")
+    expect(page).to have_content("View My Contacts")
+  end
+
+  it("goes to the phones page") do
+    visit('/contacts') do
+      click_link(", John")
+      expect(page).to have_content('Phone Num')
+      expect(page).to have_content('at')
+    end
+  end
+
 end
